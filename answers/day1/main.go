9d797b87078f4d86f74d102365e3eca43e3d1b4f
Steven White <swhitewvu24@gmail.com>
2018-12-04 11:41:51 -0500

    add solution for day 1 pt 2

--- answers/day1/main.go
+++ answers/day1/main.go
@@ -12,17 +12,39 @@ func main() {
 	var err error
 	var lines []string
 
-	sum := 0
-
 	if lines, err = util.GetInputLines(1); err != nil {
 		log.Fatalf("failed to load input file: %s", err.Error())
 	}
 
+	fmt.Println("sum: ", findSum(lines))
+	fmt.Println("first dup: ", findDup(lines))
+}
+
+func findSum(lines []string) int {
+	sum := 0
 	for _, line := range lines {
 		sum = process(sum, line)
 	}
+	return sum
+}
 
-	fmt.Println("sum: ", sum)
+func findDup(lines []string) int {
+	var dup *int
+	sum := 0
+	frequencies := map[int]bool{0: true}
+
+main:
+	for {
+		for _, line := range lines {
+			sum = process(sum, line)
+			if frequencies[sum] {
+				dup = &sum
+				break main
+			}
+			frequencies[sum] = true
+		}
+	}
+	return *dup
 }
 
 func process(sum int, line string) int {
